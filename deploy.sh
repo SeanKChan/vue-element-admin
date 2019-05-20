29acbad1470e9d8a9f4b227e2045edf90120ef1c
chenkai22 <chenkai22@meituan.com>
2019-05-20 09:35:40 +0800

    feat: merge master

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:PanJiaChen/vue-element-admin.git master:gh-pages
+git push -f git@github.com:ZYBANK-FE/vue-element-admin.git master:gh-pages
 
 cd -
