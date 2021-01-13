ca12d313c3a21388f664a443b47d5d9c4e0fb5ab
Chan <chan597463138@gmail.com>
2021-01-13 13:26:25 +0800

    feat: 开发tagify组件

--- src/components/Tagify/index.vue
+++ src/components/Tagify/index.vue
@@ -1,26 +1,90 @@
 <template>
-  <textarea name="tagify" rows="3" cols="40" />
+  <textarea name="mix" rows="3" cols="40" />
 </template>
 <script>
 import Tagify from '@yaireo/tagify/dist/tagify.min.js'
 
 export default {
   name: 'Tagify',
-  props: {},
+  model: {
+    prop: 'output',
+    event: 'change'
+  },
+  props: {
+    input: String,
+    whiteList: {
+      type: Array,
+      required: true
+    },
+    output: String
+  },
   data() {
     return {
-      input: null,
+      content: '',
       instance: null
     }
   },
   mounted() {
-    this.input = document.querySelector('[name=tagify]')
+    this.instanceInit()
   },
   methods: {
     instanceInit() {
-      this.instance = new Tagify(this.input, {
-
+      // 1. 生成实例
+      this.instance = new Tagify(document.querySelector('[name=mix]'), {
+        mode: 'mix',
+        placeholder: '使用@键入变量',
+        enforceWhitelist: true,
+        pattern: /@/,
+        tagTextProp: 'value',
+        whitelist: this.transformWhiteList(this.whiteList),
+        dropdown: {
+          enabled: 1,
+          position: 'text',
+          mapValueTo: 'value',
+          highlightFirst: true
+        }
       })
+      // 2. 加载输入模板
+      this.instance.loadOriginalValues(this.input)
+      // 3. 绑定事件
+      this.instance.on('input', e => {
+        /**
+         * prefix 前缀
+         * value 前缀后的值
+         * textContent 输出值
+         */
+        const { prefix, value, textContent } = e.detail
+        if (prefix === '@') {
+          this.instance.dropdown.show.call(this.instance, value)
+        }
+        this.$emit('change', textContent)
+      })
+    },
+    transformWhiteList(arr) {
+      return arr.map(o => {
+        if (_.isString(o)) {
+          return {
+            value: o
+          }
+        }
+        return o
+      })
+    },
+    getOriginalInputValue() {
+      let tmp = this.instance.DOM.originalInput.value
+      tmp = tmp.replace(/(\[\[.*?\]\])/g, (match) => {
+        return match.replace(/(\{.*?\})/g, (m) => {
+          try {
+            const json = JSON.parse(m)
+            return _.get(json, 'value')
+          } catch (e) {
+            console.warn('模板参数有误: ', m)
+          }
+          return m
+        })
+      })
+      tmp = tmp.replace(/\s*/, '')
+      return tmp
     }
   }
 }
--- src/views/components-demo/vue-tagify.vue
+++ src/views/components-demo/vue-tagify.vue
@@ -1,85 +1,33 @@
 <template>
   <div class="components-container">
     <div class="tag-group">
-      <el-tag v-for="tag in tags" :key="tag" effect="plain" :color="getRandomColor()">{{ tag }}</el-tag>
+      <el-tag v-for="tag in whiteList" :key="tag" effect="plain" :color="getRandomColor()">{{ tag }}</el-tag>
     </div>
-    <textarea name="mix" />
-    <pre>{{ originalInputValue }}</pre>
-    <div>
-      <el-button size="mini">transform</el-button>
-      <pre>{{ transformInputValue }}</pre>
-    </div>
+    <Tagify ref="tagify" v-model="output" :white-list="whiteList" :input="content" />
+    <pre>组件输入值：{{ output }}</pre>
+    <pre>组件模板值: {{ outputTmp }}</pre>
   </div>
 </template>
 <script>
-import Tagify from '@yaireo/tagify/dist/tagify.min.js'
-
-const VARIABLE = ['行内客户号', '客户姓名', '交易对手名称', '上游交易数据']
+import Tagify from '@/components/Tagify'
 
 export default {
   name: 'VueTagifyDemo',
+  components: {
+    Tagify
+  },
   data() {
     return {
-      instance: null,
       content: '[[行内客户号]] 你好',
-      originalInputValue: '',
-      transformInputValue: '',
-      tags: VARIABLE
+      whiteList: ['行内客户号', '客户姓名', '交易对手名称', '上游交易数据'],
+      output: '',
+      outputTmp: ''
     }
   },
-  mounted() {
-    const input = document.querySelector('[name=mix]')
-    // init Tagify script on the above inputs
-    this.instance = new Tagify(input, {
-      //  mixTagsInterpolator: ["{{", "}}"],
-      mode: 'mix', // <--  Enable mixed-content
-      placeholder: '使用@键入变量',
-      enforceWhitelist: true,
-      pattern: /@/, // <--  Text starting with @ or # (if single, String can be used here)
-      tagTextProp: 'value', // <-- the default property (from whitelist item) for the text to be rendered in a tag element.
-      // Array for initial interpolation, which allows only these tags to be used
-      whitelist: VARIABLE.map(o => {
-        return {
-          value: o
-        }
-      }),
-      // transformTag: this.transformTag,
-      dropdown: {
-        enabled: 1,
-        position: 'text', // <-- render the suggestions list next to the typed text ("caret")
-        mapValueTo: 'value', // <-- similar to above "tagTextProp" setting, but for the dropdown items
-        highlightFirst: true // automatically highlights first sugegstion item in the dropdown
-      },
-      callbacks: {
-        // add: console.log, // callback when adding a tag
-        // remove: console.log // callback when removing a tag
-      }
-    })
-
-    this.instance.loadOriginalValues(this.content)
-    this.originalInputValue = this.content
-
-    // A good place to pull server suggestion list accoring to the prefix/value
-    this.instance.on('input', (e) => {
-      const { prefix, value, tagify } = e.detail
-      if (prefix === '@') {
-        if (value.length >= 1) {
-          this.instance.dropdown.show.call(this.instance, value)
-        }
-      }
-      console.log('mix-mode "input" event value: ', e.detail)
-      this.originalInputValue = tagify.DOM.originalInput.value
+  watch: {
+    output() {
+      this.outputTmp = this.$refs['tagify'].getOriginalInputValue()
     }
-    )
-      .on('change', (e) => {
-        // console.log('change', e)
-      })
-      .on('add', function(e) {
-        // console.log('add', e)
-      })
-      .on('dropdown:show', (e) => {
-        // console.log('dropdown:show', e)
-      })
   },
   methods: {
     getRandomColor() {
@@ -94,19 +42,15 @@ export default {
     },
     transformTag(tagData) {
       tagData.style = '--tag-bg:' + this.getRandomColor()
-      if (tagData.value.toLowerCase() === 'shit') {
-        tagData.value = 's✲✲t'
-      }
-    },
-    transfromOriginalValue() {
-
+      // if (tagData.value.toLowerCase() === '行内客户号') {
+      //   tagData.value = '行内客户号✲*'
+      // }
     }
   }
 }
 
 </script>
 <style lang="scss">
-@import "~@yaireo/tagify/src/tagify.scss";
 
 .tag-group {
   height: 60px;
